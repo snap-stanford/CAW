df4b69b4a5d9a6f31ce4adb9ad8b410ff6c1ac8c
ywangdr <ywangdr@stanford.edu>
2020-11-08 18:42:06 -0800

    sync with private repo

--- sample.py
+++ sample.py
@@ -23,8 +23,10 @@ def seq_binary_sample_one(ngh_binomial_prob):
     seg_idx = 0
     for idx in range(len(ngh_binomial_prob)-1, -1, -1):
         a = a_l_seg[seg_idx]
-        if seg_idx >= seg_len:
+        seg_idx += 1 # move one step forward
+        if seg_idx >= seg_len:  # regenerate a batch of new random values
             a_l_seg = np.random.random((seg_len,))
+            seg_idx = 0  # and reset the seg_idx
         if a < ngh_binomial_prob[idx]:
             # print('=' * 50)
             # print(a, len(ngh_binomial_prob) - idx, len(ngh_binomial_prob),
